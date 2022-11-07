2dd7b559d167eaa53e4e9a52bd1c2b2f470a93be
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2022-11-07 12:58:20 +0700

    load_data.sql

---
+++ dbt/load_data.sql
@@ -0,0 +1,52 @@
+CREATE USER demo_user WITH PASSWORD 'demo_password';
+
+CREATE TABLE taxi_zone_lookup
+(
+    locationid   INTEGER NOT NULL,
+    borough      VARCHAR NOT NULL,
+    zone         VARCHAR,
+    service_zone VARCHAR
+);
+
+COPY taxi_zone_lookup
+    FROM '/tmp/taxi_zone_lookup.csv'
+    DELIMITER ',' CSV HEADER;
+
+ALTER TABLE taxi_zone_lookup
+    OWNER TO demo_user;
+
+CREATE TABLE yellow_tripdata_sample_2019_01
+(
+    vendor_id           INTEGER NOT NULL,
+    pickup_datetime     TIMESTAMP WITHOUT TIME ZONE,
+    dropoff_datetime    TIMESTAMP WITHOUT TIME ZONE,
+    passenger_count     INTEGER NOT NULL,
+    pickup_location_id  DECIMAL NOT NULL,
+    dropoff_location_id DECIMAL NOT NULL,
+    fare_amount         DECIMAL NOT NULL
+);
+
+COPY yellow_tripdata_sample_2019_01
+    FROM '/tmp/yellow_tripdata_sample_2019_01.csv'
+    DELIMITER ',' CSV HEADER;
+
+ALTER TABLE yellow_tripdata_sample_2019_01
+    OWNER TO demo_user;
+
+CREATE TABLE yellow_tripdata_sample_2019_02
+(
+    vendor_id           INTEGER NOT NULL,
+    pickup_datetime     TIMESTAMP WITHOUT TIME ZONE,
+    dropoff_datetime    TIMESTAMP WITHOUT TIME ZONE,
+    passenger_count     INTEGER NOT NULL,
+    pickup_location_id  DECIMAL NOT NULL,
+    dropoff_location_id DECIMAL NOT NULL,
+    fare_amount         DECIMAL NOT NULL
+);
+
+COPY yellow_tripdata_sample_2019_02
+    FROM '/tmp/yellow_tripdata_sample_2019_02.csv'
+    DELIMITER ',' CSV HEADER;
+
+ALTER TABLE yellow_tripdata_sample_2019_02
+    OWNER TO demo_user;
